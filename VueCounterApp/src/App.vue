ffe5f725ee0e11c6e3359141b95377c6bdc18564
TalhaAzhar500 <143387303+TalhaAzhar500@users.noreply.github.com>
2023-11-02 16:21:18 +0500

    Add files via upload

---
+++ VueCounterApp/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const increment = () => {
+  count.value = count.value + 1;
+};
+
+const decrement = () => {
+  if (count.value === 0) {
+    return;
+  }
+  count.value = count.value - 1;
+};
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The Current Count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="decrement">-</button>
+      <button @click="increment">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+div {
+  text-align: center;
+}
+button {
+  padding: 10px 15px;
+  margin: 20px;
+}
+</style>
